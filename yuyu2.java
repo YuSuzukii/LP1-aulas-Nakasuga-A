d54e0faafd020d7b077bdf7a546ea4535251ff86
YuSuzukii <126773140+YuSuzukii@users.noreply.github.com>
2023-03-16 08:43:37 -0300

    Add files via upload

---
+++ yuyu2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class yuyu2 {
+    public class yuyu {
+        static char[][] tabuleiro = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+        static char p1;
+        static char p2;
+        static int cont = 0;
+        static char i;
+        static char j;
+        static int jogadaColuna, jogadaLinha;
+        public static boolean vitoria;
+        static Scanner le = new Scanner(System.in);
+        public static void main (String[]args) {
+         }
+    }
+}
+
